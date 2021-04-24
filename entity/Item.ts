69dff6337493e2a22289fd6e0c8cf11ca0fce3be
Jaime Rivera <jriverarojas@gmail.com>
2021-04-24 14:22:20 -0400

    Item

--- entity/Item.ts
+++ entity/Item.ts
@@ -12,36 +12,39 @@
  * Last modified  :
  */
 import {
-    Entity,
-    PrimaryGeneratedColumn,
-    Column,
-    Unique
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  OneToMany
 } from 'typeorm';
 
 import { PxpEntity } from '../../../lib/pxp';
-
+import Discount from './Discount';
 @Entity({ name: 'ttr_item' })
 
 
 export default class Item extends PxpEntity {
 
-    @PrimaryGeneratedColumn({name:'item_id'})
-    itemId: number;
+  @PrimaryGeneratedColumn({ name: 'item_id' })
+  itemId: number;
 
 
-    @Column({name:'type', type:'varchar', length: 200, nullable: true })
-    type: string;
+  @Column({ name: 'type', type: 'varchar', length: 200, nullable: true })
+  type: string;
 
 
-    @Column({name:'price', type:'numeric', nullable: true })
-    price: string;
+  @Column({ name: 'price', type: 'numeric', nullable: true })
+  price: string;
 
-    @Column({name:'available_yn', type:'varchar', length: 80, nullable: true })
-    availableYN: string;
+  @Column({ name: 'available_yn', type: 'varchar', length: 80, nullable: true })
+  availableYN: string;
 
-    @Column({name:'quantity', type:'numeric', nullable: true })
-    quantity: string;
+  @Column({ name: 'quantity', type: 'numeric', nullable: true })
+  quantity: string;
 
-    @Column({name:'unit', type:'varchar', length: 15, nullable: true })
-    unit: string;
+  @Column({ name: 'unit', type: 'varchar', length: 15, nullable: true })
+  unit: string;
+
+  @OneToMany(() => Discount, discount => discount.item)
+  discounts: Discount[];
 }
