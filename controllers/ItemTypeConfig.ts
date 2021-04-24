5019f096cdaafb71f5554fc47fac0cda150bcd70
GhostReader <red.ghost1314@gmail.com>
2021-04-24 14:11:46 -0400

    Created entity for item_type_config

---
+++ controllers/ItemTypeConfig.ts
@@ -0,0 +1,39 @@
+/**
+ * Kplian Ltda 2020
+ *
+ * MIT
+ *
+ * Person Controller
+ *
+ * @summary Account Status Type Controller
+ * @author Bellido Hilarion
+ *
+ * Created at     : 2021-03-10 18:55:38
+ * Last modified  :
+ */
+import { EntityManager } from 'typeorm';
+
+import {
+    Controller,
+    Get,
+    Post,
+    Put,
+    Delete,
+    Patch,
+    Route,
+    StoredProcedure,
+    DbSettings,
+    ReadOnly,
+    Authentication,
+    Permission,
+    Model, __, Log
+} from '../../../lib/pxp';
+
+import ItemTypeConfig from '../entity/ItemTypeConfig';
+
+@Model('trading-nd/ItemTypeConfig')
+class ProductType extends Controller {
+
+}
+
+export default ItemTypeConfig;
